456ac0ef7224e40d7c7ea71689852b07258f058e
Abdulrahman Dawoud <dawoud.abdulrahman.2004@gmail.com>
2023-05-01 19:48:03 +0600

    added csv download data

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from sensor.views import SensorGraphView
+from sensor.views import SensorGraphView, download_csv
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("sensor.api.urls")),
     path("graph/", SensorGraphView.as_view(), name="graph"),
+    path("data_csv", download_csv, name="data_csv"),
 ]
--- sensor/views.py
+++ sensor/views.py
@@ -1,3 +1,7 @@
+import csv
+
+from django.http import HttpResponse
+from django.views import View
 from django.views.generic import ListView
 
 from sensor.models import Sensor
@@ -6,3 +10,17 @@ from sensor.models import Sensor
 class SensorGraphView(ListView):
     queryset = Sensor.objects.all()
     template_name = "sensor_v2.html"
+
+
+def download_csv(request):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="sensor_data.csv"'
+
+    writer = csv.writer(response)
+    writer.writerow(['start_time', 'end_time', 'time_interval', 'distance'])
+
+    sensors = Sensor.objects.all()
+    for sensor in sensors:
+        writer.writerow([sensor.start_time, sensor.end_time, sensor.time_interval, sensor.distance])
+
+    return response
